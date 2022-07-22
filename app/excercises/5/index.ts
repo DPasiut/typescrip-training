e3ed8ed005d4ed6e68a82896088b3d8bb47654a9
DPasiut <domel1162@gmail.com>
2022-07-22 14:47:42 +0200

    exercise 5

--- app/excercises/5/index.ts
+++ app/excercises/5/index.ts
@@ -90,9 +90,7 @@ export function logPerson(person: Person) {
     console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
-export type PartUser = Partial<User>;
-
-export function filterUsers(persons: Person[], criteria: PartUser): User[] {
+export function filterUsers(persons: Person[], criteria: Partial<Omit<User, "type">>): User[] {
     return persons.filter(isUser).filter(( user) => {
         const criteriaKeys = Object.keys(criteria) as (keyof Omit<User, "type">)[];
         return criteriaKeys.every((fieldName) => {
